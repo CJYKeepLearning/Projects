59ab80ff0532edd69e887832e9d08b53f7890cb2
cjy <1292585150@qq.com>
2021-05-31 13:57:15 +0800

    SpringBoot_foling_community

--- community/src/main/java/com/foling/community/controller/HelloController.java
+++ community/src/main/java/com/foling/community/controller/HelloController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
+    @GetMapping("he")
+    public String hello(@RequestParam(name = "name",defaultValue = "hhh")String name, Model model){
         model.addAttribute("name",name);
         return "hello";
     }
