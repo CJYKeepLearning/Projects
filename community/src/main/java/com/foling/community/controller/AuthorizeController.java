4c5d5ffaf3003dc38e900b7fde3f199067b66080
cjy <1292585150@qq.com>
2021-07-20 15:54:50 +0800

    实现获取Github用户信息

--- community/src/main/java/com/foling/community/controller/AuthorizeController.java
+++ community/src/main/java/com/foling/community/controller/AuthorizeController.java
@@ -1,28 +1,44 @@
 package com.foling.community.controller;
 
 import com.foling.community.dto.AccessTokenDTO;
+import com.foling.community.dto.GithubUser;
 import com.foling.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@PropertySource({"classpath:application.properties"})
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("ed649d9ab02352975f0c");
-        accessTokenDTO.setClient_secret("fc83df741a4ae4fbe748bc2b9964e08cf9702166");
-        githubProvider.getAccessTokenDTO(new AccessTokenDTO());
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
         return "index";
     }
 }
--- community/src/main/java/com/foling/community/provider/GithubProvider.java
+++ community/src/main/java/com/foling/community/provider/GithubProvider.java
@@ -1,5 +1,6 @@
 package com.foling.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.foling.community.dto.AccessTokenDTO;
 import com.foling.community.dto.GithubUser;
 import okhttp3.*;
@@ -8,30 +9,48 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 @Component
 public class GithubProvider {
-    public String getAccessTokenDTO(AccessTokenDTO access){
-         MediaType JSON = MediaType.get("application/json; charset=utf-8");
-
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json;charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(JSON,"json");
+        //构建请求体
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+
+        //构建请求 将请求体进行绑定到请求中
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
+
+        //进行响应
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        } catch (IOException e) {
+            String str = response.body().string();
+            String token = str.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public GithubUser githubUser(String accessToken){
+    public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("http://api.github.com/user?access_token")
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+accessToken)
                 .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (IOException e){
+
+        }
+        return null;
     }
+
 
 
 }
